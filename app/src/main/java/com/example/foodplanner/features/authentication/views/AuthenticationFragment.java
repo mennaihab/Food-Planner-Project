7ea5c2e846744c11a8048532c1b1fa1de2b25614
Moustafa Essam <drsh_201122@yahoo.com>
2023-02-07 18:05:42 +0200

    add authentication screen

--- app/src/main/java/com/example/foodplanner/features/authentication/views/AuthenticationFragment.java
+++ app/src/main/java/com/example/foodplanner/features/authentication/views/AuthenticationFragment.java
@@ -3,13 +3,22 @@ package com.example.foodplanner.features.authentication.views;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.foodplanner.R;
+import com.example.foodplanner.core.helpers.TextSpan;
+import com.example.foodplanner.core.utils.SpanUtils;
+import com.example.foodplanner.core.utils.TextUtils;
 import com.example.foodplanner.features.common.views.WindowPainter;
 
 public class AuthenticationFragment extends Fragment {
@@ -29,6 +38,36 @@ public class AuthenticationFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        windowPainter.clearStatusBarColor();
+        windowPainter.setStatusBarVisibility(false);
+
+        setupLogin(view.findViewById(R.id.login_tv));
+        setupDisclaimer(view.findViewById(R.id.disclaimer_tv));
+
+        view.<Button>findViewById(R.id.skip_btn).setOnClickListener(e->{
+            Navigation.findNavController(e).navigate(AuthenticationFragmentDirections.actionAuthenticationToHomeMeal());
+        });
+
+        view.<Button>findViewById(R.id.signup_btn).setOnClickListener(e->{
+            Navigation.findNavController(e).navigate(AuthenticationFragmentDirections.actionAuthenticationToSignup());
+        });
+    }
+
+    private void setupLogin(TextView textView) {
+        SpannableString spannableString = SpanUtils.createSpannable("Already have an account? Login",
+                TextSpan.of(SpanUtils.createClickableSpan(e -> {
+                    Navigation.findNavController(e).navigate(AuthenticationFragmentDirections.actionAuthenticationToLogin());
+                }), 25)
+        );
+        textView.setText(spannableString);
+        TextUtils.makeClickable(textView);
+    }
+
+    private void setupDisclaimer(TextView textView) {
+        SpannableString spannableString = SpanUtils.createSpannable("By using FOODA, you agree to our Privacy Notice and Terms of Use",
+                TextSpan.of(SpanUtils.createClickableSpan(e -> {}), 33, 47, Spanned.SPAN_INCLUSIVE_INCLUSIVE),
+                TextSpan.of(SpanUtils.createClickableSpan(e -> {}), 52)
+        );
+        textView.setText(spannableString);
+        TextUtils.makeClickable(textView);
     }
 }
