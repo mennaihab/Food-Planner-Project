9d5f82638a66b06a72d8ca5dd68f2a004fa27203
Moustafa Essam <drsh_201122@yahoo.com>
2023-02-18 20:37:09 +0200

    add settings manager

---
+++ app/src/main/java/com/example/foodplanner/features/common/helpers/SettingsManagerImpl.java
@@ -0,0 +1,34 @@
+package com.example.foodplanner.features.common.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.foodplanner.features.common.services.SettingsManager;
+
+public class SettingsManagerImpl implements SettingsManager {
+    private static final String SETTINGS_FILE = "app_settings";
+    private static final String HAS_SHOWN_LANDING = "HAS_SHOWN_LANDING";
+
+    private final SharedPreferences preferences;
+
+    public SettingsManagerImpl(SharedPreferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public static SettingsManagerImpl create(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SETTINGS_FILE, Context.MODE_PRIVATE);
+        return new SettingsManagerImpl(sharedPreferences);
+    }
+
+    @Override
+    public Boolean hasShownLanding() {
+        String value = preferences.getString(HAS_SHOWN_LANDING, null);
+        if (value == null) return null;
+        return Boolean.valueOf(value);
+    }
+
+    @Override
+    public void setHasShownLanding(boolean value) {
+        preferences.edit().putString(HAS_SHOWN_LANDING, String.valueOf(value)).apply();
+    }
+}
---
+++ app/src/main/java/com/example/foodplanner/features/common/services/SettingsManager.java
@@ -0,0 +1,6 @@
+package com.example.foodplanner.features.common.services;
+
+public interface SettingsManager {
+    Boolean hasShownLanding();
+    void setHasShownLanding(boolean value);
+}
